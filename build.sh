7abee9776c99b09a0d3bf04637da50679d3c055a
robbycandra <robbycandra.mail@gmail.com>
2017-02-05 11:50:30 +0700

    Detect Realsense

--- build.sh
+++ build.sh
@@ -15,12 +15,19 @@ elif [[ $version == *"15.04"* ]] ; then
     sudo dpkg -i cuda-repo-ubuntu1504_7.5-18_amd64.deb
     rm cuda-repo-ubuntu1504_7.5-18_amd64.deb
     sudo apt-get update
+elif [[ $version == *"16.04"* ]] ; then
+    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+    rm cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+    sudo apt-get update
 else
-    echo "Don't use this on anything except 14.04 or 15.04"
+    echo "Don't use this on anything except 14.04 or 15.04 or 16.04"
     exit
 fi
 
-sudo apt-get install -y cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev openjdk-7-jdk freeglut3-dev libglew-dev cuda-7-5 libsuitesparse-dev libeigen3-dev zlib1g-dev libjpeg-dev
+sudo apt-get install -y g++ python doxygen graphviz 
+
+sudo apt-get install -y cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev default-jdk freeglut3-dev libglew-dev cuda libsuitesparse-dev libeigen3-dev zlib1g-dev libjpeg-dev
 
 #Installing Pangolin
 git clone https://github.com/stevenlovegrove/Pangolin.git
@@ -37,19 +44,34 @@ cd OpenNI2
 make -j8
 cd ..
 
+## move to /opt/openni2 then
+
+#sudo ./install.sh
+#cd Packaging
+#./ReleaseVersion.py x64
+#cd ../..
+#echo "source /opt/openni2/OpenNIDevEnvironment" >> ~/.bashrc
+#sudo ln -s /lib/x86_64-linux-gnu/libudev.so.1.6.4 /lib/x86_64-linux-gnu/libudev.so.0
+#sudo usermod -a -G video $USER
+
 #Actually build ElasticFusion
 cd ../Core
 mkdir build
 cd build
 cmake ../src
+#cmake -G "Eclipse CDT4 - Unix Makefiles" ../src
 make -j8
+
 cd ../../GPUTest
 mkdir build
 cd build
 cmake ../src
+#cmake -G "Eclipse CDT4 - Unix Makefiles" ../src
 make -j8
+
 cd ../../GUI
 mkdir build
 cd build
 cmake ../src
+#cmake -G "Eclipse CDT4 - Unix Makefiles" ../src
 make -j8
